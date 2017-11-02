d43d7cccc6240db70f4ab2ab632a814201562194
lukasz341 <lukasz341@tlen.pl>
2017-11-02 20:59:03 +0100

    first project in node.js

---
+++ index.js
@@ -0,0 +1,21 @@
+process.stdin.on('readable', function() {
+    
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch (instruction) {
+            case 'exit': 
+                process.stdout.write('Quitting app!\n'); //kończenie pracy programu
+                process.exit();
+                break;
+            case 'node-version':
+                console.log(process.version);  // wyświetlenie wersji node js
+                break;
+            case 'language':
+                console.log(process.env.LANG);// język systemowy użytkownika
+                break;
+            default:
+                process.stdout.write('Wrong instruction!\n');
+        };
+    }
+});
